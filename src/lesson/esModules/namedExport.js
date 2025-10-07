bf53e8acd28aa7a7bb6bb1efc32046fae13ed49f
janjan519 <207494690+janjan519@users.noreply.github.com>
2025-10-07 17:07:43 +0900

    add:section9 done

--- src/lesson/esModules/namedExport.js
+++ src/lesson/esModules/namedExport.js
@@ -1,22 +1,13 @@
 // ここにコードを書きながら確認しましょう！
 
-
-
-
-
-
-
-
-
-
-
-
 // 練習問題
 // 問題 1
 const nameFn = () => {
-  return 'Gizumo taro';
+  return "Gizumo taro";
 };
 
 const calcFn = (num) => {
   return num * 10;
 };
+
+export { nameFn, calcFn };
--- src/lesson/esModules/namedImport.js
+++ src/lesson/esModules/namedImport.js
@@ -1,15 +1,8 @@
 // ここにコードを書きながら確認しましょう！
 
-
-
-
-
-
-
-
-
-
-
-
 // 練習問題
 // 問題 1
+import { nameFn, calcFn } from "./namedExport.js";
+
+console.log(nameFn());
+console.log(calcFn(10));
