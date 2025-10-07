0d84e52eb63bea51f12f1a84c0a0cfe72e49806e
janjan519 <207494690+janjan519@users.noreply.github.com>
2025-10-07 14:51:23 +0900

    first commit

---
+++ src/lesson/defaultArgument.js
@@ -0,0 +1,12 @@
+// ここにコードを書きながら確認しましょう！
+function argFn(arg) {
+  console.log(arg);
+}
+
+argFn();
+
+// 練習問題
+// 問題 1
+function discountRateCalc(price, rate) {
+  return price * (1 - rate * 0.01);
+}
