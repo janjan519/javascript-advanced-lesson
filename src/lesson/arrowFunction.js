8ecceae739120d513a388748a1fac331ff51bd8e
janjan519 <207494690+janjan519@users.noreply.github.com>
2025-10-07 16:27:30 +0900

    add:section7 done

--- src/lesson/arrowFunction.js
+++ src/lesson/arrowFunction.js
@@ -1,18 +1,6 @@
 // ここにコードを書きながら確認しましょう！
 
-
-
-
-
-
-
-
-
-
-
-
 // 練習問題
 // 問題 1
-const testFn = function(name) {
-  return `Gizumo ${name}`;
-};
+const testFn = (name) => `Gizumo ${name}`;
+console.log(testFn("ishikawa"));
