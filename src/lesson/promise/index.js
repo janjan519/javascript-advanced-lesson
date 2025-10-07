2e02cce7b8d995ea72894fda95757c01e4466fe5
janjan519 <207494690+janjan519@users.noreply.github.com>
2025-10-07 18:42:52 +0900

    add:section11 done

--- src/lesson/promise/index.js
+++ src/lesson/promise/index.js
@@ -1,15 +1,15 @@
 // ここにコードを書きながら確認しましょう！
 
-
-
-
-
-
-
-
-
-
-
-
 // 練習問題
 // 問題 1
+const returnPromise = () => {
+  return new Promise((_resolve, reject) => {
+    setTimeout(() => {
+      reject("エラーが発生しました");
+    }, 3000);
+  });
+};
+
+returnPromise().catch((err) => {
+  console.log(err);
+});
