f1613043d3088f27cd7c690ae37546ed8efa981e
janjan519 <207494690+janjan519@users.noreply.github.com>
2025-10-07 16:23:04 +0900

    add:section6 done

--- src/lesson/spreadSyntax.js
+++ src/lesson/spreadSyntax.js
@@ -1,35 +1,31 @@
 // ここにコードを書きながら確認しましょう！
+const numbers = [1, 2, 3, 4, 5];
 
-
-
-
-
-
-
-
-
-
-
+console.log(numbers); // [1, 2, 3, 4, 5]
+console.log(...numbers); // 1 2 3 4 5
 
 // 練習問題
 // 問題 1
-const packageManager = ['npm', 'yarn', 'pnpm'];
+const packageManager = ["npm", "yarn", "pnpm"];
 
 function arrayFn(array) {
-  const framework = ['Vue', 'React', 'Svelte'];
+  const framework = ["Vue", "React", "Svelte"];
+  const mergeArray = [...framework, ...array];
+  return mergeArray;
 }
 
 console.log(arrayFn(packageManager));
 
 // 問題 2
 const objectA = {
-  language: 'JavaScript',
-  framework: 'Vue',
+  language: "JavaScript",
+  framework: "Vue",
   version: 2,
 };
 
 const objectB = {
   version: 3,
+  routingLibrary: "Vue Router",
 };
 
-console.log();
+console.log({ ...objectA, ...objectB });
